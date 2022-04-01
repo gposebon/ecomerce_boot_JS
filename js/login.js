87d1e26801c5c4dd7b70b03571ac1b3e2675b831
gposebon <gposebon@gmail.com>
2022-04-01 14:55:37 -0300

    agrego shop.js

--- js/login.js
+++ js/login.js
@@ -2,28 +2,36 @@
 
 const usuarios = [
     {
-        email: "pepito@gmail.com",
+        nombre: "pepito",
         password: 1234
     },
     {
-        email: "roberto@gmail.com",
+        nombre: "roberto",
         password: 1234
     },
     {
-        email: "alfredo@gmail.com",
+        nombre: "alfredo",
         password: 1234
     }
 ]
+
+nombre = JSON.parse(localStorage.getItem('usuario'))
+document.getElementById("usuario").innerHTML = nombre
+console.log(nombre)
 
 const compruebaLogin = () => {
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
 
     usuarios.forEach(usuario =>{
-        if(email == usuario.email && password == usuario.password) {
+        if(email == usuario.nombre && password == usuario.password) {
             window.location.href = "../index.html";
         }else {
             document.getElementById("error-login").innerHTML = `<h3>Datos Incorrectos</h3>`
         }
+
+        localStorage.setItem('usuario', JSON.stringify(usuario.nombre))
     })
+
+
 }
--- js/shop.js
+++ js/shop.js
@@ -1,88 +1,50 @@
 'use strict'
 
-// let carrito = []
-// let cantidad = 0
-// let opcion = 0
-// let totalCompra = 0
-
-// const comprobarStock = (stock, cantidad) => {
-//     if(stock < cantidad) {
-//         return false
-//     } else {
-//         return true
-//     }
-
-// }
-
-// const restaStock = (stock, cantidad) => {
-//     stock -= cantidad
-//     return stock
-// }
-
-// const calculeTotalPrecio = (cantidad, precio) => {
-//      return cantidad * precio
-// }
-
-
-// const compra = (productos, productoElegido, cantidad) =>{
-//     productos.forEach( producto => {
-//         if(producto.id == productoElegido){
-            
-//             let comprueba = comprobarStock(producto.stock, cantidad)
-            
-//             if(comprueba == true) {
-//                 totalCompra += calculeTotalPrecio(cantidad, producto.precio)
-//                 carrito.push(`${producto.nombre} $${producto.precio}`)
-//                 producto.stock = restaStock(producto.stock, cantidad)
-                
-//             }else {
-//                 console.log(`No hay mas de este producto`)
-//             }
-//         }
-//     })
-// }
+let carrito = []
+let cantidad = 0
+let totalCompra = 0
 
 const productos = [
     {
         nombre: "Lechuga",
         precio: 80,
         comentarioImagen: "Lechuga",
-        imagen: "./img/lechuga.jpg",
+        imagen: "../img/lechuga.jpg",
         stock: 3
     },
     {
         nombre: "Morron",
         precio: 150,
         comentarioImagen: "Morron",
-        imagen: "./img/morron.jpg",
+        imagen: "../img/morron.jpg",
         stock: 3
     },
     {
         nombre: "Acelga",
         precio: 100,
         comentarioImagen: "Acelga",
-        imagen: "./img/acelga.jpg",
+        imagen: "../img/acelga.jpg",
         stock: 3
     },
     {
         nombre: "Papa",
         precio: 120,
         comentarioImagen: "Papa",
-        imagen: "./img/papa.jpg",
+        imagen: "../img/papa.jpg",
         stock: 3
     },
     {
         nombre: "Cebolla",
         precio: 70,
         comentarioImagen: "Cebolla",
-        imagen: "./img/cebolla.jpg",
+        imagen: "../img/cebolla.jpg",
         stock: 3
     },
     {
         nombre: "Calabaza",
         precio: 200,
         comentarioImagen: "Calabaza",
-        imagen: "./img/calabaza.jpg",
+        imagen: "../img/calabaza.jpg",
         stock: 3
     }
 ]
@@ -93,24 +55,66 @@ const generaCards = (productos) => {
         cardsAMostrar += `
         <div class="col">
             <div class="card mx-auto" style="width: 18rem;">
-                <div class="mb-3 subtituloProductos">
-                    ${producto.nombre}
+                <div class="subtituloProductos mb-3">
+                ${producto.nombre}
                 </div>
-                <img src="${producto.imagen}" class="card-img-top" alt="${producto.comentarioImagen}">
+                <img src="${producto.imagen}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <div class="p-2 border-top border-bottom">
-                        <label for="numeroPrecio"><h6>Precio: </h6></label>
-                        <label class="precio" for="numeroPrecio">${producto.precio} </label> Pesos. 
-                    </div>  
-                    <div class="linkCompra border-top">
-                        <a class="nav-link" href="./html/shop.html">Ir a compras</a>
-                    </div>
+                <div class="p-2 border-top border-bottom">
+                    <label for="numeroPrecio"><h6>Precio: </h6></label>
+                    <label class="precio" for="numeroPrecio">${producto.precio}</label> Pesos. 
+                </div>  
+                <div class="p-2">
+                    <label for="cantidadKilos">Cantidad:</label>
+                    <input class="inputKilos" type="number" name="kilos" id="${producto.id}"> Kilos.
+                </div>
+                <div class="linkCompra border-top">
+                    <button onclick="compra(${producto.id})">Comprar</button>
+                </div>
                 </div>
             </div>    
         </div>
         `
     });
     mostrarCards(cardsAMostrar)
+}
+
+const comprobarStock = (stock, cantidad) => {
+    if(stock < cantidad) {
+        return false
+    } else {
+        return true
+    }
+
+}
+
+const restaStock = (stock, cantidad) => {
+    stock -= cantidad
+    return stock
+}
+
+const calculeTotalPrecio = (cantidad, precio) => {
+     return cantidad * precio
+}
+
+let productosEnCarrito = JSON.parse(localStorage.getItem('carrito'))
+
+const compra = (productoID) => {
+    const productoElegido = productos.find(producto => productoID == producto.id)
+
+    cantidad = document.getElementById(productoID).value
+
+    let comprueba = comprobarStock(productoElegido.stock, cantidad)
+
+    if(comprueba == true) {
+        totalCompra += calculeTotalPrecio(cantidad, productoElegido.precio)
+        carrito.push(productoElegido)
+        productoElegido.stock = restaStock(productoElegido.stock, cantidad)
+        document.getElementById("contador-carrito").innerHTML = carrito.length
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    } else {
+        console.log("No queda mas de este producto")
+    }
 }
 
 const mostrarCards = (cards) => {
